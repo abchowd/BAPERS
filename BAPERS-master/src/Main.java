b359f2b7219523ace15b3261b750fd0b76bd7919
Abul <abul.chowdhury@city.ac.uk>
2021-03-11 19:34:12 +0000

    Initial commit

---
+++ BAPERS-master/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        LoginPage lp = new LoginPage("Login Page");
+        lp.setVisible(true); // add a size relative to screen size
+
+
+
+    }
+}
